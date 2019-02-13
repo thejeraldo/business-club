7630ce66ab242146bf47e3fb07a49c0ec47f1f38
Jerald Abille <jerald@moonshine.ae>
2019-02-13 23:43:56 +0400

    First commit.

---
+++ Business Club/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Business Club
+//
+//  Created by Jerald Abille on 2/13/19.
+//  Copyright © 2019 Jerald Abille. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
